9dc021e233070e2c2544670cd252922d76277488
cipher <dilsurbet@hmail.com>
2024-06-24 17:32:29 +0500

    6oy2darsuyishiyangi

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container container">
+        <p>
+          Barcha Huquqlar Himoyalangan <a href="#">DK</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
